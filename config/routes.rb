8fea16cd437c3623ca52f0a4f0e84fdb4e10bf89
Junya Sato <s1200015junya@yahoo.co.jp>
2018-11-03 21:04:53 +0900

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  # root "application#hello"
+  # root "static_pages#hello"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
